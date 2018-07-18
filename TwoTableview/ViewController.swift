963b3cb25b8f35982e3edc4a7d081b4340d98a13
Shanthakumar <Shanthakumar@ngws37.local>
2018-07-18 15:32:43 +0530

    Initial Commit

---
+++ TwoTableview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwoTableview
+//
+//  Created by Shanthakumar on 18/07/18.
+//  Copyright © 2018 Shanthakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
