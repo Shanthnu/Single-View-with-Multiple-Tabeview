914713781486b381715eecc8dc34c8eea397f9ac
Shanthakumar <Shanthakumar@ngws37.local>
2018-07-18 15:54:24 +0530

    test

--- TwoTableview/ViewController.swift
+++ TwoTableview/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableview1: UITableView!
+    @IBOutlet weak var tableview2: UITableView!
+    @IBOutlet weak var tableview3: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +26,49 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == tableview1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CellOne") as! CellOne
+            return cell
+        }else if tableView == tableview2 {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CellTwo") as! CellTwo
+            return cell
+
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CellThree") as! CellThree
+            return cell
+
+        }
+    }
+}
+
+
+class CellTwo : UITableViewCell {
+    override func awakeFromNib() {
+        self.backgroundColor = UIColor.green
+    }
+}
+
+class CellOne : UITableViewCell {
+    override func awakeFromNib() {
+        self.backgroundColor = UIColor.blue
+
+    }
+}
+
+class CellThree : UITableViewCell {
+    override func awakeFromNib() {
+        self.backgroundColor = UIColor.yellow
+    }
+}
